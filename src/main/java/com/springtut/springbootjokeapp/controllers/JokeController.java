84f1e1985253a856e42619d48349da297034978d
Timo Peters <tpeters@novomind.com>
2020-09-01 10:52:48 +0200

    Add Thymeleaf view to display joke property. Closes #8. Closes #9

--- src/main/java/com/springtut/springbootjokeapp/controllers/JokeController.java
+++ src/main/java/com/springtut/springbootjokeapp/controllers/JokeController.java
@@ -19,6 +19,6 @@ public class JokeController {
   @RequestMapping({"/",""})
   public String showJoke(Model model){
     model.addAttribute("joke", jokeService.getJoke());
-    return "chucknorris";
+    return "index";
   }
 }
